71ec5ed8fbf8b1e74880b3e5d0778ef622fb4bfd
WORD559 <josh.barrass.work@gmail.com>
2019-01-06 23:23:07 +0000

    Added new tests

--- test/test_Map.py
+++ test/test_Map.py
@@ -22,6 +22,15 @@ class MapTests(unittest.TestCase):
         m.set_tile((5,3), 11, False)
         self.assertEqual(m[5,3], (11, False))
 
+    def test_setitem(self):
+        m = Map.new((10,5), 0)
+        m[5,2] = (12, True)
+        self.assertEqual(m[5,2], (12, True))
+        m[5,3] = 13
+        self.assertEqual(m[5,3], (13, False))
+        m[5,4] = True
+        self.assertEqual(m[5,4], (0, True))
+
     def test_save_and_open_file(self):
         m = Map.new((10,5), 0)
         m.set_tile((5,2), 12, True)
@@ -35,3 +44,32 @@ class MapTests(unittest.TestCase):
     def tearDown(self):
         if os.path.isfile("tempmap.tmp"):
             os.remove("tempmap.tmp")
+
+    def test_len(self):
+        m = Map.new((10,5), 0)
+        self.assertEqual(len(m), 50)
+
+    def test_iterating(self):
+        m = Map.new((2,2), 0)
+        m[0,0] = 1
+        l = [i for i in m]
+        self.assertEqual(l, [(1,False), (0,False),
+                             (0,False), (0,False)])
+
+    def test_get_slice(self):
+        m = Map.new((3,3), 0)
+        m[0,0] = (1, True)
+        self.assertEqual(m[0:2,0], ([1,0],[True,False]))
+        self.assertEqual(m[0,0:2], ([1,0],[True,False]))
+        self.assertEqual(m[0:2,0:2], ([[1,0],[0,0]],[[True,False],[False,False]]))
+
+    def test_set_slice(self):
+        m = Map.new((3,3), 0)
+        m[0:2,0] = (1, True)
+        self.assertEqual(m[0:2,0], ([1,1],[True,True]))
+        m[0,0:2] = (1, True)
+        self.assertEqual(m[0,0:2], ([1,1],[True,True]))
+        m[0:2,0:2] = (2, False)
+        self.assertEqual(m[0:2,0:2], ([[2,2],[2,2]],[[False,False],[False,False]]))
+        
+        
